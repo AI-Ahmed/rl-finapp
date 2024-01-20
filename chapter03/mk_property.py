a1002951a7e201929195d67fc5df3894e2fb18e4
AI-Ahmed <n.is.ccit@gmail.com>
2024-01-20 16:47:16 +0200

    Update chapter03 - Process1

--- chapter03/mk_property.py
+++ chapter03/mk_property.py
@@ -3,20 +3,29 @@ import jax.numpy as jnp
 
 from dataclasses import dataclass
 
+k = jax.random.PRNGKey(42)
 
-@dataclass
+
+@dataclass(frozen=True)
 class Process1:
     @dataclass
     class State:
         price: int
 
+        @property
+        def dtype(self):
+            return jnp.dtype(object)
+
     level_param: int  # level to which price mean-reverts
     alpha1: float  # strength of mean-reversion (non-negative value)
-    k: jnp.ndarray = jax.random.PRNGKey(42)
 
     def up_prob(self, state: State) -> float:
         return 1. / (1 + jnp.exp(-self.alpha1 * (self.level_param - state.price)))
 
     def next_state(self, state: State) -> State:
-        up_move: int = jax.random.binomial(self.k, 1, self.up_prob(state), dtype=jnp.float32)
+        up_move: int = jax.random.binomial(k, 1, self.up_prob(state), dtype=jnp.float32)
         return Process1.State(price=state.price + up_move * 2 - 1)
+
+    @property
+    def dtype(self):
+        return jnp.dtype(object)
--- chapter03/utils.py
+++ chapter03/utils.py
@@ -28,32 +28,3 @@ def process1_price_traces(
             simulation(process, start_state),
             time_steps + 1)), float)
         for _ in range(num_traces)])
-
-
-# Using Jax
-def jx_simulation(process: Process1, start_state: Process1.State, time_steps: int) -> jnp.ndarray:
-    def update_state(carry, _):
-        return process.next_state(carry), None
-
-    final_state, _ = jax.lax.scan(update_state, start_state, xs=None, length=time_steps)
-
-    return jnp.concatenate([start_state[None, :], final_state])
-
-
-@jax.jit
-def jx_process1_price_traces(
-        start_price: int,
-        level_param: int,
-        alpha1: float,
-        time_steps: int,
-        num_traces: int) -> jnp.ndarray:
-    def jx_loop(i):
-        return jx_simulation(process, start_state, time_steps)
-
-    process = Process1(level_param=level_param, alpha1=alpha1)
-    start_state = Process1.State(price=start_price)
-
-    traces = jax.vmap(lambda _: jx_simulation(process, start_state, time_steps))(jnp.arange(0, num_traces,
-        dtype=jnp.int32))
-
-    return traces
